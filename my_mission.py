024c37fa55389d9598b85f7bca650ba02141fa1d
Phoneria <90638359+Phoneria@users.noreply.github.com>
2024-03-22 14:55:18 +0300

    Update my_mission.py

--- my_mission.py
+++ my_mission.py
@@ -12,7 +12,10 @@ x_waypoints = [39.87241710265067, 39.87244660651398,  39.87209255932302]
 y_waypoints = [32.73220615380094, 32.732491271469094, 32.73248806789979]
 
 
+
+target_altitude = 10
 async def run():
+
     drone = System()
     await drone.connect(system_address="udp://:14540")
 
@@ -21,6 +24,28 @@ async def run():
         if state.is_connected:
             print(f"-- Connected to drone!")
             break
+
+    print("Waiting for drone to have a global position estimate...")
+    async for health in drone.telemetry.health():
+        if health.is_global_position_ok and health.is_home_position_ok:
+            print("-- Global position estimate OK")
+            break
+
+    print("DRONE STARTED TO TAKEOFF")
+
+    await drone.action.set_takeoff_altitude(target_altitude)
+
+
+    print("ARMING")
+    await drone.action.arm()
+
+    print("TAKING OFF")
+    print("TAKEOFF ALTITUDE : ", target_altitude)
+    await drone.action.takeoff()
+
+    await asyncio.sleep(target_altitude + 10) #  Wait For takeoff
+    
+    print("TAKEOFF COMPLETED")
 
     print_mission_progress_task = asyncio.ensure_future(
         print_mission_progress(drone))
